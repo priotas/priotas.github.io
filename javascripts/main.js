fedffac0eb1ced6cbf2a281eaa4b8efe4267cf5b
madflow <madflow@web.de>
2016-09-18 16:57:15 +0200

    static

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,30 @@
-console.log('This would be the main JS file.');
+var root = 'https://api.github.com/orgs/priotas';
+
+$(document).ready(function() {
+
+
+
+    $("#repos").typed({
+        strings: ['git log -1', 'ps faux | grep dump', 'du -ck'],
+        typeSpeed: 200,
+        loop: true,
+        cursorChar: "|",
+        callback: function() {
+            //console.log($('#repos').text());
+        }
+    });
+
+    /** 
+    $.get(root, function(data) {
+
+        var typings = [];
+
+        $.get(data.repos_url, function(repos) {
+
+            $.each(repos, function(key, repo) {
+            });
+
+        });
+    });
+    */
+});
