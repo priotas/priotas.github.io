7a7539e37ab03969a52f161248a7389e61547a30
madflow <madflow@web.de>
2016-09-18 21:21:14 +0200

    :S

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,10 +5,21 @@ $(document).ready(function() {
 
 
     $("#repos").typed({
-        strings: ['git log -1', 'ps faux | grep dump', 'du -ck'],
-        typeSpeed: 200,
+        strings: [
+            'grep -ri console.log *',
+            'git reset --hard', 
+            'ps faux | grep dump', 
+            'du -ck', 
+            'man man',
+            'npm install webpack -g',
+            'lynx https://www.priotas.de',
+            'bin/behat -vv --profile ci --tags "~@performance"',
+            'git clone <a href="https://github.com/priotas/twig-qrcode-extension">https://github.com/priotas/twig-qrcode-extension</a>'
+        ],
+        typeSpeed: 100,
         loop: true,
         cursorChar: "|",
+        shuffle: true,
         callback: function() {
             //console.log($('#repos').text());
         }
